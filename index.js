991ea2abeb150fa053d5bbf915fb092d73d93998
daniel <danielgonzalez252@gmail.com>
2023-03-12 21:43:46 -0400

    Lab Finished

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName)
+{
+    const messagesArray = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+        messagesArray.push(message);
+    }
+
+    return messagesArray;
+}
+
+function countDown(num)
+{
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+
+
+}
